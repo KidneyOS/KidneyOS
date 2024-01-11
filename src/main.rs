030be5a30dfcbac1286b1d1ae3e814d559c68528
Matthew Toohey <contact@mtoohey.com>
2024-01-11 18:50:53 -0500

    use simpler cfg checks

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#![cfg_attr(target_family = "kidneyos", no_std)]
-#![cfg_attr(target_family = "kidneyos", no_main)]
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg_attr(target_os = "none", no_main)]
 
-#[cfg(target_family = "kidneyos")]
+#[cfg(target_os = "none")]
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[cfg_attr(target_family = "kidneyos", no_mangle)]
+#[cfg_attr(target_os = "none", no_mangle)]
 fn main() {
     loop {}
 }
