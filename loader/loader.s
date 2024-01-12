37b9975bb1db20c040638eedf7696605d43f34ae
Matthew Toohey <contact@mtoohey.com>
2024-01-11 19:47:04 -0500

    loader: refactor printing into a puts function

--- loader/loader.s
+++ loader/loader.s
@@ -4,25 +4,51 @@
 .section .text
 .global _start
 _start:
-	lea si, hello # Load address of string.
-	mov ah, 0x0E # Specifies 'Write Character in TTY mode' operation.
+	# Set up data and stack segments to both start from zero so we can refer
+	# to absolute addresses in the following setup code.
+	xorw ax, ax
+	mov ds, ax
+	mov ss, ax
 
-.loop:
-	lodsb # Loads value from address in si into al when in legacy mode.
+	# Start the stack (which grows downward) at 1K.
+	# NOTE: This may need to be increased if loader complexity grows.
+	mov esp, 0x400
 
-	# Break if we've gotten to the '\0'.
-	or al, al
-	jz halt
-
-	int 0x10 # BIOS interrupt 0x10 (Video Services).
-	jmp .loop
+	call puts
+	.asciz "Hello, world!\r\n"
+	call puts
+	.asciz "Halting...\r\n"
 
 halt:
 	cli
 	hlt
 
-hello:
-	.asciz "Hello world!\r\n"
 
+# puts prints the null-terminated string whose data is contained in the code
+# immediately following the call to puts to the screen using BIOS interrupts. If
+# you want the cursor to end at the start of the next line, your string should
+# end with "\r\n".
+puts:
+	# Use return address as the address of the string to print.
+	mov si, [esp]
+	mov ah, 0xE # Specify "Display Character" operation for the BIOS interrupts below.
+
+.puts_loop:
+	lodsb # Load value at address in si into al and increment si.
+
+	# Return if this is the null-terminator.
+	test al, al
+	jz .puts_done
+
+	int 0x10 # Trigger video services BIOS interrupt.
+	jmp .puts_loop
+
+.puts_done:
+	# Use the updated value of si (which is now past the string) as the
+	# return address.
+	mov [esp], si
+	ret
+
+# Include magic number to mark sector as bootable.
 	. = _start + 510
-	.word 0xAA55 # Magic number to mark sector as bootable.
+	.word 0xAA55
