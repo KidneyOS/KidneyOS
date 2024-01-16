c320e6851ec1bfb140566d729e115448d288644f
Matthew Toohey <contact@mtoohey.com>
2024-01-15 23:49:07 -0500

    add stuff for building in docker

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,14 +13,14 @@
     };
   };
 
-  outputs = { nixpkgs, utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, utils, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           overlays = [ (import rust-overlay) ];
           inherit system;
         };
-        inherit (pkgs) bochs gdb grub2 mkShell qemu rustfmt rust-analyzer
+        inherit (pkgs) bochs gdb gnumake grub2 mkShell qemu rust-analyzer
           rust-bin unixtools xorriso;
         inherit (unixtools) xxd;
         rust = rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
@@ -30,19 +30,32 @@
         }).stdenv.cc;
       in
       {
-        devShells.default = mkShell {
-          packages = [
-            bochs
-            gdb
-            grub2
-            i686-cc
-            qemu
-            rust
-            rustfmt
-            rust-analyzer
-            xorriso
-            xxd
-          ];
+        containerImages.build = pkgs.dockerTools.buildNixShellImage {
+          name = "kidneyos-builder";
+          tag = "latest";
+          drv = self.devShells.x86_64-linux.build;
+        };
+
+        devShells = {
+          build = mkShell {
+            packages = [
+              gnumake
+              grub2
+              i686-cc
+              rust
+              xorriso
+            ];
+          };
+
+          default = self.devShells.${system}.build.overrideAttrs (oldAttrs: {
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+              bochs
+              gdb
+              qemu
+              rust-analyzer
+              xxd
+            ];
+          });
 
           CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_LINKER = "${i686-cc.targetPrefix}cc";
           CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_RUNNER = "${qemu}/bin/qemu-i386";
