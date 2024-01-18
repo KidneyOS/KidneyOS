18d772bb371f9812c897eb5ff5f3643e09f02a0f
Matthew Toohey <contact@mtoohey.com>
2024-01-18 12:33:24 -0500

    fix iso creation on non-x86 platforms

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,14 +20,26 @@
           overlays = [ (import rust-overlay) ];
           inherit system;
         };
-        inherit (pkgs) bochs gdb gnumake grub2 mkShell mtools qemu rust-analyzer
+        inherit (pkgs) bochs gdb gnumake mkShell mtools qemu rust-analyzer
           rust-bin unixtools xorriso;
         inherit (unixtools) xxd;
         rust = rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
-        i686-cc = (import nixpkgs {
+        i686-pkgs = import nixpkgs {
           crossSystem = "i686-linux";
           inherit system;
-        }).stdenv.cc;
+        };
+        i686-cc = i686-pkgs.stdenv.cc;
+        grub2 =
+          if system == "x86_64-linux"
+          then pkgs.grub2
+          else
+            pkgs.runCommand "grub2-mkrescue-i686-cross"
+              {
+                buildInputs = [ pkgs.makeWrapper ];
+              } ''
+              makeWrapper ${pkgs.grub2}/bin/grub-mkrescue $out/bin/grub-mkrescue \
+                --add-flags "--directory=${i686-pkgs.grub2}/lib/grub/i386-pc"
+            '';
       in
       {
         packages.kidneyos-builder = pkgs.dockerTools.buildNixShellImage {
