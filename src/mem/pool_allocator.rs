d7c406e08413c72dd7588d5c4196cdd7e95d7211
HLeiTR <sy.hung@mail.utoronto.ca>
2024-03-02 21:07:49 -0500

    [+] Attempted to make the bitmap self contained within Pool Allocator

--- src/mem/pool_allocator.rs
+++ src/mem/pool_allocator.rs
@@ -1,15 +1,15 @@
 use core::{
     alloc::{Layout, Allocator, AllocError},
     ptr::NonNull,
-    cell::UnsafeCell,
+    mem::size_of,
+    slice,
+    ptr,
 };
 
-use alloc::boxed::Box;
-use alloc::vec;
 
 pub struct PoolAllocator<const N: usize> {
     region: NonNull<[u8]>,
-    bitmap: UnsafeCell<Box<[u8]>>,
+    // bitmap: UnsafeCell<Box<[u8]>>,
 }
 
 impl<const N: usize> PoolAllocator<N>{
@@ -23,12 +23,44 @@ impl<const N: usize> PoolAllocator<N>{
         // Calculate the required bitmap size in bytes, because each stores an u8
 
         // Round up the division to the nearest whole number
+        // This unit is in bytes
         let bitmap_size = (region.len() / N).div_ceil(8);
 
-        // Initialize the bitmap vector with zeros
-        let bitmap = UnsafeCell::new(vec![0u8; bitmap_size].into_boxed_slice());
+        // To allow the first block to always store the size of bitmap
+        assert!(N >= size_of::<usize>());
 
-        Self { region, bitmap }
+        // Calculate how many blocks the bitmap occupies
+        let bitmap_blocks = bitmap_size.div_ceil(N);
+        let total_blocks = region.len().div_ceil(N);
+
+        // Ensure the region is large enough for the size of bitmap + bitmap_blocks
+        // + at least 1 extra block is available
+        assert!(total_blocks >= bitmap_blocks + 1 + 1);
+
+        unsafe {
+            let region_ptr = region.as_ptr();
+
+            // The first block is used to store how large the bitmap is
+            ptr::write(region_ptr as *mut usize, bitmap_size);
+
+            // Initialize the bitmap area to zero, which is 1 block away from the start
+            ptr::write_bytes((region_ptr as *mut u8).add(N), 0, bitmap_size);
+
+            // Get a mutable slice of the region where the bitmap is stored
+            let bitmap_slice = slice::from_raw_parts_mut((region_ptr as *mut u8).add(N), bitmap_size);
+
+            // Mark the blocks used by the bitmap as used in the bitmap
+            for i in 0..bitmap_blocks {
+                let byte_index = i / 8;
+                let bit_index = i % 8;
+                if byte_index < bitmap_size {
+                    // Set the bit to mark the block as used
+                    bitmap_slice[byte_index] |= 1 << bit_index;
+                }
+            }
+        }
+
+        Self { region }
     }
 
 }
@@ -52,23 +84,35 @@ unsafe impl<const N: usize> Allocator for PoolAllocator<N> {
         let mut start_index = None;
         let mut free_count = 0;
 
-        let bitmap = unsafe { self.bitmap.get().as_mut() }.unwrap();
+        // Obtain the bitmap from the region
+        // Size of the bitmap is in the first block
+        let bitmap_size = unsafe {
+            *(self.region.as_ptr() as *const usize)
+        };
+
+        // Get a pointer to the start of the bitmap, the bitmap starts 1 block away from the start
+        let bitmap_ptr = unsafe {
+            (self.region.as_ptr() as *const u8).add(N)
+        };
 
         // Search for a contiguous sequence of free blocks
-        for (index, &bit) in bitmap.iter().enumerate(){
-            for bit_pos in 0..8 {
-                if bit & (1 << bit_pos) == 0 {
-                    free_count += 1;
-                    start_index.get_or_insert(index * 8 + bit_pos);
-                    if free_count >= blocks_required {
-                        // Found a suitable region
-                        break;
+        for i in 0..bitmap_size {
+            unsafe {
+                for j in 0..8 {
+                    let bit = bitmap_ptr.add(i).read() & (1 << j);
+                    if bit == 0 {
+                        free_count += 1;
+                        if free_count == blocks_required {
+                            start_index = Some(i * 8 + j - free_count + 1);
+                            break;
+                        }
+                    } else {
+                        free_count = 0;
                     }
-                } else {
-                    // Reset the counter and start index if a used block is found
-                    start_index = None;
-                    free_count = 0;
                 }
+            }
+            if start_index.is_some() {
+                break;
             }
         }
 
@@ -88,11 +132,13 @@ unsafe impl<const N: usize> Allocator for PoolAllocator<N> {
 
         // Update the bitmap to mark the blocks as used
         unsafe {
-            let bitmap_ptr = self.bitmap.get().as_mut().unwrap(); // Get a mutable reference to the bitmap
+            // Get the bitmap pointer
+            let bitmap_ptr = (self.region.as_ptr() as *mut u8).add(N);
+
             for i in 0..blocks_required {
                 let byte_index = (start_bit + i) / 8;
                 let bit_pos = (start_bit + i) % 8;
-                (*bitmap_ptr)[byte_index] |= 1 << bit_pos;
+                bitmap_ptr.add(byte_index).write(bitmap_ptr.add(byte_index).read() | (1 << bit_pos));
             }
         }
 
@@ -114,25 +160,26 @@ unsafe impl<const N: usize> Allocator for PoolAllocator<N> {
 
         let start_addr = region_slice.as_ptr() as usize; // Start address of the region
 
-        // Checks the bitmap and see if that bit is flipped
-        let bitmap = self.bitmap.get().as_mut().unwrap();
+        // Get a pointer to the start of the bitmap, the bitmap starts 1 block away from the start
+        let bitmap_ptr = (self.region.as_ptr() as *mut u8).add(N);
 
+        // Find out which block it belongs to
         let start_bit = (ptr.as_ptr() as usize - start_addr) / N;
 
         // Sanity check: We should have layout.size() / N blocks starting from start_bit
-        for i in 0..layout.size() / N {
+        for i in 0..layout.size().div_ceil(N) {
             let byte_index = (start_bit + i) / 8;
             let bit_pos = (start_bit + i) % 8;
-            if bitmap[byte_index] & (1 << bit_pos) == 0 {
+            if bitmap_ptr.add(byte_index).read() & (1 << bit_pos) == 0 {
                 panic!("Double free detected");
             }
         }
 
         // Mark the blocks as free
-        for i in 0..layout.size() / N {
+        for i in 0..layout.size().div_ceil(N) {
             let byte_index = (start_bit + i) / 8;
             let bit_pos = (start_bit + i) % 8;
-            bitmap[byte_index] &= !(1 << bit_pos);
+            bitmap_ptr.add(byte_index).write(bitmap_ptr.add(byte_index).read() & !(1 << bit_pos));
         }
     }
 }
