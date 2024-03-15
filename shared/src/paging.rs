824f4a308599cb913eb8428335a4d4fb80ba68aa
Matthew Toohey <contact@mtoohey.com>
2024-03-14 23:10:39 -0400

    use PSE to dramatically speed up paging setup

--- shared/src/paging.rs
+++ shared/src/paging.rs
@@ -4,7 +4,7 @@
 use crate::{
     mem::{
         phys::{kernel_data_start, kernel_end, kernel_start, main_stack_top, trampoline_heap_top},
-        virt, OFFSET, PAGE_FRAME_SIZE,
+        virt, HUGE_PAGE_SIZE, OFFSET, PAGE_FRAME_SIZE,
     },
     video_memory::{VIDEO_MEMORY_BASE, VIDEO_MEMORY_SIZE},
 };
@@ -17,6 +17,7 @@ use core::{
     ops::{Deref, DerefMut},
     ptr::NonNull,
 };
+use lazy_static::lazy_static;
 
 const PAGE_DIRECTORY_LEN: usize = PAGE_FRAME_SIZE / size_of::<PageDirectoryEntry>();
 
@@ -112,6 +113,16 @@ struct PageTableEntry {
     page_frame: u20,
 }
 
+#[bitfield(u32)]
+struct VirtualAddress {
+    #[bits(22..=31, r)]
+    page_directory_index: u10,
+    #[bits(12..=21, r)]
+    page_table_index: u10,
+    #[bits(0..=11, r)]
+    offset: u12,
+}
+
 /// Wraps lower-level paging data structures.
 pub struct PageManager<A: Allocator> {
     root: NonNull<PageDirectory>,
@@ -121,14 +132,6 @@ pub struct PageManager<A: Allocator> {
 
 const PAGE_DIRECTORY_LAYOUT: Layout = Layout::new::<PageDirectory>();
 const PAGE_TABLE_LAYOUT: Layout = Layout::new::<PageTable>();
-
-// PERF: Set page_size when frames_len is long enough for it to be usable if PSE
-// is supported. Make sure we enable PSE before doing this.
-
-// PERF: Use Rc to hold page tables for regions used in multiple page frames?
-// Will increase complexity and might have to consider how this would play with
-// locks, interrupts, etc... If PSE is supported and we set page_size, this
-// might not be necessary.
 
 impl<A: Allocator> PageManager<A> {
     pub fn new_in(alloc: A, alloc_addr_to_phys_offset: usize) -> Self {
@@ -206,15 +209,14 @@ impl<A: Allocator> PageManager<A> {
     /// there's nothing in the TLB for the virtual addresses included in this
     /// mapping, the mapping may take effect without the call to `load`.
     pub unsafe fn map(&mut self, phys_addr: usize, virt_addr: usize, write: bool, user: bool) {
-        #[bitfield(u32)]
-        struct VirtualAddress {
-            #[bits(22..=31, r)]
-            page_directory_index: u10,
-            #[bits(12..=21, r)]
-            page_table_index: u10,
-            #[bits(0..=11, r)]
-            offset: u12,
-        }
+        assert!(
+            phys_addr % PAGE_FRAME_SIZE == 0,
+            "phys_addr was not page-frame-aligned"
+        );
+        assert!(
+            virt_addr % PAGE_FRAME_SIZE == 0,
+            "virt_addr was not page-frame-aligned"
+        );
 
         let page_directory = self.root.as_mut();
         let virt_addr = VirtualAddress::new_with_raw_value(virt_addr as u32);
@@ -273,6 +275,42 @@ impl<A: Allocator> PageManager<A> {
             .with_page_frame(u20::new(phys_frame));
     }
 
+    /// Like map, except with length `HUGE_PAGE_SIZE`. `virt_addr` must have an
+    /// alignment of `HUGE_PAGE_SIZE`, but `phys_addr` only needs to be aligned
+    /// to `PAGE_FRAME_SIZE`.
+    ///
+    /// # Safety
+    ///
+    /// Same as `map`.
+    pub unsafe fn huge_map(&mut self, phys_addr: usize, virt_addr: usize, write: bool, user: bool) {
+        assert!(
+            phys_addr % HUGE_PAGE_SIZE == 0,
+            "phys_addr was not properly aligned"
+        );
+        assert!(
+            virt_addr % HUGE_PAGE_SIZE == 0,
+            "virt_addr was not properly aligned"
+        );
+
+        let page_directory = self.root.as_mut();
+        let virt_addr = VirtualAddress::new_with_raw_value(virt_addr as u32);
+
+        let pdi = virt_addr.page_directory_index().value() as usize;
+
+        assert!(
+            !page_directory[pdi].present(),
+            "virtual address {:#X} was already mapped",
+            virt_addr.raw_value()
+        );
+
+        page_directory[pdi] = PageDirectoryEntry::default()
+            .with_present(true)
+            .with_read_write(write)
+            .with_user_supervisor(user)
+            .with_page_size(true)
+            .with_page_table_frame(u20::new((phys_addr / PAGE_FRAME_SIZE) as u32));
+    }
+
     /// Maps virtual addresses from `virt_start..(virt_start + len)` to the
     /// physical addresses `phys_start..(phys_start + len)`. `phys_start` and
     /// `virt_start` must both be page-frame-aligned. In other words, they must
@@ -305,10 +343,29 @@ impl<A: Allocator> PageManager<A> {
             "len was not a multiple of PAGE_FRAME_SIZE"
         );
 
-        for offset in (0..len).step_by(PAGE_FRAME_SIZE) {
-            let phys_addr = phys_start + offset;
-            let virt_addr = virt_start + offset;
+        let mut phys_addr = phys_start;
+        let mut virt_addr = virt_start;
+        loop {
+            if phys_addr - phys_start >= len {
+                break;
+            }
+
+            if *PSE_ENABLED
+                && virt_addr % HUGE_PAGE_SIZE == 0
+                && phys_addr.saturating_add(HUGE_PAGE_SIZE) - phys_start <= len
+            {
+                self.huge_map(phys_addr, virt_addr, write, user);
+
+                phys_addr = phys_addr.saturating_add(HUGE_PAGE_SIZE);
+                virt_addr = virt_addr.saturating_add(HUGE_PAGE_SIZE);
+
+                continue;
+            }
+
             self.map(phys_addr, virt_addr, write, user);
+
+            phys_addr = phys_addr.saturating_add(PAGE_FRAME_SIZE);
+            virt_addr = virt_addr.saturating_add(PAGE_FRAME_SIZE);
         }
     }
 
@@ -383,6 +440,90 @@ pub unsafe fn enable() {
         mask = const MASK,
         options(nomem, nostack),
     );
+}
+
+lazy_static! {
+    static ref PSE_SUPPORTED: bool = {
+        #[bitfield(u32, default = 0)]
+        struct EFlags {
+            #[bit(21, rw)]
+            id: bool,
+        }
+
+        let eflags_diff: usize;
+        unsafe {
+            asm!(
+                "
+        pushfd // Save original EFLAGS.
+
+        // Get a copy of EFLAGS and modify it to toggle the mask bit.
+        pushfd
+        pop {0}
+        xor {0}, {mask}
+        push {0}
+
+        popfd // Move the copy into EFLAGS.
+
+        // Get a diff of what changed in EFLAGS after the attempted
+        // modification.
+        pushfd
+        pop {0}
+        xor {0}, [esp]
+
+        popfd // Restore original EFLAGS.
+                ",
+                out(reg) eflags_diff,
+                mask = const EFlags::DEFAULT.with_id(true).raw_value(),
+                options(nomem),
+            )
+        };
+
+        // If the attempted modification didn't change the id bit, then cpuid
+        // isn't supported.
+        if !EFlags::new_with_raw_value(eflags_diff as u32).id() {
+            return false;
+        }
+
+        #[bitfield(u32, default = 0)]
+        struct CPUIDEdx {
+            #[bit(3, rw)]
+            pse: bool,
+        }
+
+        #[cfg(arch = "x86")]
+        {
+            let core::arch::x86::CpuidResult { edx, .. } = unsafe { core::arch::x86::__cpuid(0) };
+            CPUIDEdx::new_with_raw_value(edx).pse()
+        }
+        #[cfg(not(arch = "x86"))]
+        unimplemented!()
+    };
+    static ref PSE_ENABLED: bool = {
+        // Check if PSE is already enabled (from the trapoline, if we're running
+        // in the kernel).
+        #[bitfield(u32, default = 0)]
+        struct CR4 {
+            #[bit(4, rw)]
+            pse: bool,
+        }
+
+        let cr4: usize;
+        unsafe { asm!("mov {}, cr4", out(reg) cr4, options(nomem, nostack)) };
+        let cr4 = CR4::new_with_raw_value(cr4 as u32);
+        if cr4.pse() {
+            // If it is, early return true.
+            return true;
+        }
+
+        if !*PSE_SUPPORTED {
+            // If not, and it's not supported, early return false.
+            return false;
+        }
+
+        // Otherwise, enable it and return true.
+        unsafe { asm!("mov cr4, {}", in(reg) cr4.with_pse(true).raw_value() as usize, options(nomem, nostack)) };
+        true
+    };
 }
 
 pub struct MappingRange {
