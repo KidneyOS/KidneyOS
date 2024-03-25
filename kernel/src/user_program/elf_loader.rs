3f697aec7701679a576936703fce425a64bc5afb
serenayu11 <serenayu39@gmail.com>
2024-03-25 17:49:27 -0400

    Add segment validation for each program header.

--- kernel/src/user_program/elf_loader.rs
+++ kernel/src/user_program/elf_loader.rs
@@ -1,5 +1,6 @@
 use super::virtual_memory_area::{VmAreaStruct, VmFlags};
 use alloc::vec::Vec;
+use kidneyos_shared::mem::{OFFSET, PAGE_FRAME_SIZE};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -64,6 +65,8 @@ const PF_W: u32 = 2; // Writable.
 const PF_R: u32 = 4; // Readable.
 
 const ELF_MAGIC_NUMBER: [u8; 4] = [0x7F, b'E', b'L', b'F'];
+const PHYS_BASE: *const () = OFFSET as *const ();
+const PGMASK: usize = OFFSET - 1;
 
 // Function to verify ELF header
 fn verify_elf_header(header: &Elf32Ehdr) -> Result<(), ElfError> {
@@ -102,6 +105,61 @@ fn verify_elf_header(header: &Elf32Ehdr) -> Result<(), ElfError> {
     Ok(())
 }
 
+#[allow(unused)]
+fn is_kernel_vaddr(vaddr: *const ()) -> bool {
+    unsafe { vaddr >= PHYS_BASE }
+}
+
+#[allow(unused)]
+fn is_user_vaddr(vaddr: *const ()) -> bool {
+    unsafe { vaddr < PHYS_BASE }
+}
+
+fn validate_segment(phdr: &Elf32Phdr, file_data: &[u8]) -> bool {
+    // p_offset and p_vaddr must have the same page offset.
+    if (phdr.p_offset as usize & PGMASK) != (phdr.p_vaddr as usize & PGMASK) {
+        return false;
+    }
+
+    // p_offset must point within FILE.
+    if phdr.p_offset as usize > file_data.len() {
+        return false;
+    }
+
+    // p_memsz must be at least as big as p_filesz.
+    if phdr.p_memsz < phdr.p_filesz {
+        return false;
+    }
+
+    // The segment must not be empty.
+    if phdr.p_memsz == 0 {
+        return false;
+    }
+
+    // The virtual memory region must both start and end within the
+    // user address space range.
+    if !is_user_vaddr(phdr.p_vaddr as *const ()) {
+        return false;
+    }
+    if !is_user_vaddr((phdr.p_vaddr as usize + phdr.p_memsz as usize) as *const ()) {
+        return false;
+    }
+
+    // The region cannot "wrap around" across the kernel virtual
+    // address space.
+    if (phdr.p_vaddr as usize + phdr.p_memsz as usize) < (phdr.p_vaddr as usize) {
+        return false;
+    }
+
+    // Disallow mapping page 0.
+    if (phdr.p_vaddr as usize) < (PAGE_FRAME_SIZE as usize) {
+        return false;
+    }
+
+    // It's okay.
+    true
+}
+
 // Main function to load the ELF binary
 #[allow(unused)]
 fn load_elf(elf_data: &[u8]) -> Result<(), ElfError> {
@@ -122,7 +180,7 @@ fn load_elf(elf_data: &[u8]) -> Result<(), ElfError> {
                 .cast::<Elf32Phdr>()
         };
 
-        if ph.p_type == SegmentType::Load as u32 {
+        if (ph.p_type == SegmentType::Load as u32) && validate_segment(ph, elf_data) {
             let vm_start = ph.p_vaddr as usize;
             let vm_end = vm_start + ph.p_memsz as usize;
 
