ff5b031c3e08d276858d4a1fbbc6238101f70910
serenayu11 <serenayu39@gmail.com>
2024-03-25 01:15:30 -0400

    Fix clippy errors in virtual_memory_area.rs, deleted unused VmOperations.

--- kernel/src/user_program/virtual_memory_area.rs
+++ kernel/src/user_program/virtual_memory_area.rs
@@ -1,19 +1,8 @@
-use modular_bitfield::prelude::*;
-
 #[allow(unused)]
 pub trait VmOperations {
     fn open(&self, area: &VmAreaStruct); // This function is invoked when the given memory area is added to an address space. Such as when it is first set up or when it's inherited during a fork.
     fn close(&self, area: &VmAreaStruct); // This function is invoked when the given memory area is removed from an address space. Like during process termination or when unloading a shared library.
-    fn nopage(&self, area: &VmAreaStruct, address: usize) -> Option<Page>; // This function is invoked by the page fault handler when a page that is not present in physical memory is accessed.
-    fn populate(
-        &self,
-        area: &VmAreaStruct,
-        address: usize,
-        len: usize,
-        prot: PprotT,
-        pgoff: usize,
-        nonblock: i32,
-    ); // This function is invoked by the remap_pages() system call to prefault a new mapping. Typically to reduce the number of page faults during runtime.
+    // fn nopage(&self, area: &VmAreaStruct, address: usize) -> Option<Page>; // This function is invoked by the page fault handler when a page that is not present in physical memory is accessed.// This function is invoked by the remap_pages() system call to prefault a new mapping. Typically to reduce the number of page faults during runtime.
 }
 
 pub struct VMAOperations;
@@ -28,31 +17,12 @@ impl VmOperations for VMAOperations {
     fn close(&self, area: &VmAreaStruct) {
         // Implement close logic
     }
-
-    #[allow(unused)]
-    fn nopage(&self, area: &VmAreaStruct, address: usize) -> Option<Page> {
-        // Implement nopage logic, returning Some(Page) or None
-        None
-    }
-
-    #[allow(unused)]
-    fn populate(
-        &self,
-        area: &VmAreaStruct,
-        address: usize,
-        len: usize,
-        prot: PprotT,
-        pgoff: usize,
-        nonblock: i32,
-    ) {
-        // Implement populate logic
-    }
 }
 
 pub struct VmAreaStruct {
-    vm_start: usize, // VMA start, inclusive
-    vm_end: usize,   // VMA end, exclusive
-    flags: VmFlags,
+    pub vm_start: usize, // VMA start, inclusive
+    pub vm_end: usize,   // VMA end, exclusive
+    pub flags: VmFlags,
     // TODO: Add other necessary fields here
 }
 
@@ -67,7 +37,7 @@ pub struct VmFlags {
 }
 
 impl VmAreaStruct {
-    fn new(vm_start: usize, vm_end: usize, flags: VmFlags) -> Self {
+    pub fn new(vm_start: usize, vm_end: usize, flags: VmFlags) -> Self {
         Self {
             vm_start,
             vm_end,
