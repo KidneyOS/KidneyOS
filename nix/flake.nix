173f0ffe5dfc53af9ca6f5365e625eee49bd5000
Matthew Toohey <contact@mtoohey.com>
2024-04-02 18:00:36 -0400

    use rust-analyzer from overlay instead of from nixpkgs
    Doing things this way means the rust-analyzer version will match the
    compiler version, which is ideal.

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,8 +20,8 @@
           overlays = [ (import rust-overlay) ];
           inherit system;
         };
-        inherit (pkgs) bochs gdb gnumake mkShell mtools qemu rust-analyzer
-          rust-bin unixtools xorriso;
+        inherit (pkgs) bochs gdb gnumake mkShell mtools qemu rust-bin unixtools
+          xorriso;
         inherit (unixtools) xxd;
         rust = rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
         i686-pkgs = import nixpkgs {
@@ -72,7 +72,6 @@
             nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
               bochs
               gdb
-              rust-analyzer
               xxd
             ];
           });
