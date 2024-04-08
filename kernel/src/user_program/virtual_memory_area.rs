60755ab23d49c43cdf453010c76b1e765b14bdf0
serenayu11 <serenayu39@gmail.com>
2024-04-08 19:45:21 -0400

    Add documentations for VMA.

--- kernel/src/user_program/virtual_memory_area.rs
+++ kernel/src/user_program/virtual_memory_area.rs
@@ -1,3 +1,9 @@
+/* The function of the VMA is to define a contiguous area of virtual memory (contiguous virtual addresses) with the correct permissions such as read, write, execute,
+and whether the area is private to the process or shared with others, and the file (if any) that the region is mapped to. Operating systems with virtual memory use VMAs
+to manage memory protection, to share memory between processes, and to lazily allocate memory, among other things. When a binary file, like an ELF executable, is loaded,
+the operating system creates VMAs for its different sections, such as code, data, and BSS segments, according to the program headers in the ELF file.  */
+
+// VmOperations to perform operations related to the VMA (might implement in future).
 #[allow(unused)]
 pub trait VmOperations {
     fn open(&self, area: &VmAreaStruct); // This function is invoked when the given memory area is added to an address space. Such as when it is first set up or when it's inherited during a fork.
@@ -7,6 +13,7 @@ pub trait VmOperations {
 
 pub struct VMAOperations;
 
+// Implementations of VmOperations.
 impl VmOperations for VMAOperations {
     #[allow(unused)]
     fn open(&self, area: &VmAreaStruct) {
@@ -19,6 +26,8 @@ impl VmOperations for VMAOperations {
     }
 }
 
+/* Represents a Virtual Memory Area in an operating system's memory management system.
+It contains fields for the start and end addresses of the VMA, an instance of VmFlags which stores various flags as a bitfield.*/
 pub struct VmAreaStruct {
     pub vm_start: usize, // VMA start, inclusive
     pub vm_end: usize,   // VMA end, exclusive
@@ -26,6 +35,8 @@ pub struct VmAreaStruct {
     // TODO: Add other necessary fields here
 }
 
+/* Stores boolean flags. The flags include read, write, execute, shared, and private, each represented as a single bit.
+This allows the operating system to track the properties and permissions of different memory areas allocated to a process.*/
 #[derive(Debug, PartialEq, Eq, Default)]
 pub struct VmFlags {
     pub read: bool,
