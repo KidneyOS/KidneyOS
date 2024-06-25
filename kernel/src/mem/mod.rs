42bd794f3473c115c6554c87e5f8b9661e76515a
Timothy Du <timothydu@Timothys-MacBook-Pro-2.local>
2024-06-25 17:12:09 -0400

    Preliminary additions of dummy allocator, INCOMPLETE: Global alloc has not been adjusted, this does not work!!!

--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -1,3 +1,4 @@
+#![feature(new_uninit)]
 mod buddy_allocator;
 mod frame_allocator;
 pub mod user;
@@ -11,13 +12,18 @@ use core::{
     ops::Range,
     ptr::NonNull,
 };
-use frame_allocator::FrameAllocatorSolution;
+use std::mem;
+use frame_allocator::{CoreMapEntry, FrameAllocatorSolution, DummyAllocatorSolution};
 use kidneyos_shared::{
     mem::{virt::trampoline_heap_top, BOOTSTRAP_ALLOCATOR_SIZE, OFFSET, PAGE_FRAME_SIZE},
     println,
     sizes::{KB, MB},
 };
 use crate::mem::frame_allocator::PlacementPolicy;
+use crate::mem::frame_allocator::PlacementPolicy::NextFit;
+
+static FIRST_ALLOCATION: bool = false;
+
 
 /// # Safety
 ///
@@ -29,7 +35,7 @@ trait FrameAllocator
     /// Create a new FrameAllocator.
     /// TODO
     /// Add support for including the number of frames in the initialization
-    fn new_in(placement_policy: PlacementPolicy) -> Self;
+    fn new_in(core_map: Box<[CoreMapEntry]>, num_frames_in_system: usize, placement_policy: PlacementPolicy) -> Self;
 
     /// Allocate the specified number of frames if possible, returning a range
     /// of indices for the allocated frames.
@@ -45,11 +51,12 @@ struct FrameAllocatorWrapper{
     frame_allocator: FrameAllocatorSolution,
 }
 
+// TODO: Double check the functions in this wrapper
 impl FrameAllocatorWrapper{
-    fn new_in(start: NonNull<u8>, _max_frames: usize) -> Self {
+    fn new_in(start: NonNull<u8>, core_map: Box<[CoreMapEntry]>, num_frames_in_system: usize) -> Self {
         Self {
             start,
-            frame_allocator: FrameAllocatorSolution::default(),
+            frame_allocator: FrameAllocatorSolution::new_in(core_map, num_frames_in_system, NextFit),
         }
     }
 
@@ -72,7 +79,9 @@ impl FrameAllocatorWrapper{
 }
 
 enum KernelAllocatorState {
-    Uninitialized,
+    Uninitialized {
+        dummy_allocator: DummyAllocatorSolution,
+    },
     Initialized {
         frame_allocator: FrameAllocatorWrapper,
         subblock_allocators: Vec<(BuddyAllocator, NonNull<[u8]>), BuddyAllocator>,
@@ -100,7 +109,9 @@ macro_rules! halt {
 impl KernelAllocator {
     pub const fn new() -> KernelAllocator {
         Self {
-            state: UnsafeCell::new(KernelAllocatorState::Uninitialized),
+            state: UnsafeCell::new(KernelAllocatorState::Uninitialized{
+                dummy_allocator: DummyAllocatorSolution::new_in(0, 0),
+            })
         }
     }
 
@@ -112,7 +123,9 @@ impl KernelAllocator {
     ///
     /// This function can only be called when the allocator is uninitialized.
     pub unsafe fn init(&mut self, mem_upper: usize) {
-        let KernelAllocatorState::Uninitialized = self.state.get_mut() else {
+        let KernelAllocatorState::Uninitialized {
+            dummy_allocator
+        } = &mut *self.state.get_mut() else {
             panic!("init called while kernel allocator was already initialized");
         };
 
@@ -122,20 +135,37 @@ impl KernelAllocator {
         // The exclusive max address is given by multiplying the number of bytes
         // in a KB by mem_upper, and adding this to UPPER_MEMORY_START.
         let frames_max = UPPER_MEMORY_START.saturating_add(mem_upper * KB);
-
         let bootstrap_base = trampoline_heap_top() as *mut u8;
-
         let bootstrap_allocator = BuddyAllocator::new(NonNull::slice_from_raw_parts(
             NonNull::new_unchecked(bootstrap_base),
             BOOTSTRAP_ALLOCATOR_SIZE,
         ));
-
         let frames_base = bootstrap_base.add(BOOTSTRAP_ALLOCATOR_SIZE).cast::<u8>();
-        let max_frames = (frames_max - frames_base as usize) / PAGE_FRAME_SIZE;
+
+        // TODO: CHECK MATH!!!!
+        // Now that we know the start and end bounds for our memory region, we can set it in the
+        // dummy; initial values should be 0 if set correctly
+        let start = dummy_allocator.get_start_address();
+        let end = dummy_allocator.get_end_address();
+        assert_eq!(start, 0);
+        assert_eq!(end, 0);
+
+        dummy_allocator.set_start_address(frames_base as usize);
+        dummy_allocator.set_end_address(frames_max);
+
+        let num_frames_in_system = (frames_max - frames_base as usize) /
+            (mem::size_of::<CoreMapEntry>() + PAGE_FRAME_SIZE);
+
+        // This should ALWAYS be the first global allocation to take place - should use dummy allocator
+        let mut core_map: Box<[CoreMapEntry]> = vec![CoreMapEntry::default(); num_frames_in_system]
+                                                .into_boxed_slice();
+
+        // With the core_map not initialized, we can now initialize the actual Frame Allocator
         *self.state.get_mut() = KernelAllocatorState::Initialized {
             frame_allocator: FrameAllocatorWrapper::new_in(
-                NonNull::new(frames_base).expect("frames_base can't be null"),
-                max_frames,
+                NonNull::new(dummy_allocator.get_start_address() as *mut u8).expect("frames_base can't be null"),
+                core_map,
+                num_frames_in_system,
             ),
             subblock_allocators: Vec::new_in(bootstrap_allocator),
         };
@@ -191,7 +221,7 @@ impl KernelAllocator {
 
         assert!(leaked || subblock_allocators.is_empty());
 
-        // We can't sucessfully deinitialize because there are still references
+        // We can't successfully deinitialize because there are still references
         // to the memory that we would lose by deinitializing.
         if leaked {
             println!();
@@ -209,37 +239,46 @@ impl KernelAllocator {
 // - We never rely on allocations happening.
 unsafe impl GlobalAlloc for KernelAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let KernelAllocatorState::Initialized {
-            frame_allocator,
-            subblock_allocators,
-        } = &mut *self.state.get()
-        else {
-            halt!("alloc called before initialization of kernel allocator");
-        };
+        if FIRST_ALLOCATION {
+            /*
+            TODO:
+            1. Check that kernel is in Uninitialized state (this should only happen for the first Box Allocation)
+            2. Perform the allocation as normal
+             */
+            std::ptr::null_mut()
+        } else {
+            let KernelAllocatorState::Initialized {
+                frame_allocator,
+                subblock_allocators,
+            } = &mut *self.state.get()
+                else {
+                    halt!("alloc called before initialization of kernel allocator");
+                };
 
-        // First see if we have space in any of our existing subblock
-        // allocators, and if so return memory from there.
-        for (subblock_allocator, _) in subblock_allocators.iter() {
-            if let Ok(res) = subblock_allocator.allocate(layout) {
-                return res.as_ptr().cast::<u8>();
+            // First see if we have space in any of our existing subblock
+            // allocators, and if so return memory from there.
+            for (subblock_allocator, _) in subblock_allocators.iter() {
+                if let Ok(res) = subblock_allocator.allocate(layout) {
+                    return res.as_ptr().cast::<u8>();
+                }
             }
+
+            let Ok(region) = frame_allocator.alloc(
+                (layout.size() + layout.align() - 1 + BuddyAllocator::OVERHEAD)
+                    .next_multiple_of(PAGE_FRAME_SIZE)
+                    / PAGE_FRAME_SIZE,
+            ) else {
+                halt!("Out of virtual memory!");
+            };
+
+            let buddy_allocator = BuddyAllocator::new(region);
+            subblock_allocators.push((buddy_allocator, region));
+            buddy_allocator
+                .allocate(layout)
+                .expect("new buddy allocator created with sufficient region failed to fit planned allocation")
+                .as_ptr()
+                .cast::<u8>()
         }
-
-        let Ok(region) = frame_allocator.alloc(
-            (layout.size() + layout.align() - 1 + BuddyAllocator::OVERHEAD)
-                .next_multiple_of(PAGE_FRAME_SIZE)
-                / PAGE_FRAME_SIZE,
-        ) else {
-            halt!("Out of virtual memory!");
-        };
-
-        let buddy_allocator = BuddyAllocator::new(region);
-        subblock_allocators.push((buddy_allocator, region));
-        buddy_allocator
-            .allocate(layout)
-            .expect("new buddy allocator created with sufficient region failed to fit planned allocation")
-            .as_ptr()
-            .cast::<u8>()
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
