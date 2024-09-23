067fab7f1409aea5737bd716b177abf1f401223a
pommicket <pommicket@gmail.com>
2024-09-23 14:48:53 -0400

    Simplify VFS readdir API

--- kernel/src/fs/fat/dirent.rs
+++ kernel/src/fs/fat/dirent.rs
@@ -1,7 +1,6 @@
 use crate::block::block_core::BLOCK_SECTOR_SIZE;
 use crate::fs::fat::{error, fat::FatEntry, FatFS};
-use crate::vfs::{DirEntry, DirectoryIterator, FileInfo, INodeNum, INodeType, Result};
-use alloc::collections::BTreeMap;
+use crate::vfs::{FileInfo, INodeNum, INodeType, Result};
 use zerocopy::little_endian::{U16, U32};
 use zerocopy::{FromBytes, FromZeroes, Unaligned};
 
@@ -35,23 +34,28 @@ struct FatDirEntryLongName {
     name3: [u8; 4],
 }
 
-pub struct Directory {
-    entries: BTreeMap<u64, (String, FileInfo)>,
-    lookup: BTreeMap<String, u64>,
-    id: u64,
-}
-
 const ATTR_READ_ONLY: u8 = 0x01;
 const ATTR_HIDDEN: u8 = 0x02;
 const ATTR_SYSTEM: u8 = 0x04;
 const ATTR_VOLUME_ID: u8 = 0x08;
 const ATTR_DIRECTORY: u8 = 0x10;
-#[allow(dead_code)] // TODO : delete me
+#[allow(dead_code)] // TODO: delete once we have FAT writing (which should set this attribute)
 const ATTR_ARCHIVE: u8 = 0x20;
 const ATTR_LONG_NAME: u8 = ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID;
 
+pub struct DirEntry {
+    pub name: usize,
+    pub info: FileInfo,
+}
+
+pub struct Directory {
+    pub entries: Vec<DirEntry>,
+    pub names: Vec<u8>,
+    pub prev_name_end: usize,
+}
+
 impl Directory {
-    fn read_one_entry(&mut self, bytes: &[u8], long_name: &mut Vec<u8>) -> Result<()> {
+    fn read_one_entry(&mut self, bytes: &[u8]) -> Result<()> {
         let entry: &FatDirEntry = FatDirEntry::ref_from(bytes).unwrap();
         let attr = entry.attr;
         if attr == ATTR_LONG_NAME {
@@ -79,24 +83,27 @@ impl Directory {
             }
             // Oddly, the "long name" entries are stored in reverse.
             // So we reverse each entry, then reverse the whole thing at the end.
-            long_name.extend(utf8[..utf8_len].iter().copied().rev());
+            self.names.extend(utf8[..utf8_len].iter().copied().rev());
         } else if entry.name[0] == 0 || entry.name[0] == 0xE5 {
             // this entry is free.
             return Ok(());
         } else {
             // ordinary directory entry
-            let mut name = core::mem::take(long_name);
-            name.reverse(); // account for fact that long name entries are stored in reverse
-            let mut name =
-                String::from_utf8(name).expect("should be valid UTF-8 due to checks above");
-            if name.is_empty() {
-                // read short name
-                name = String::with_capacity(12);
-                fn read_short_name_part(name: &mut String, part: &[u8]) -> Result<()> {
+            let name = self.prev_name_end;
+            if name < self.names.len() {
+                // account for fact that long name entries are stored in reverse
+                self.names[name..].reverse();
+            } else {
+                // no long name — read short name
+                fn read_short_name_part(name: &mut Vec<u8>, mut part: &[u8]) -> Result<()> {
+                    // remove trailing spaces
+                    while part.last().is_some_and(|&c| c == b' ') {
+                        part = &part[..part.len() - 1];
+                    }
                     for c in part.iter().copied() {
-                        match c {
+                        let c = match c {
                             // technically 5 can replace E5 in the KANJI encoding
-                            5 => name.push(char::from(0xE5u8)),
+                            5 => char::from(0xE5u8),
                             b'a'..=b'z'
                             | 0..=4
                             | 6..=0x20
@@ -121,8 +128,11 @@ impl Directory {
                             // Strictly speaking this isn't correct for x >= 128,
                             // and we should instead refer to the "OEM character set".
                             // Doesn't really matter since long names are standard now.
-                            x => name.push(char::from(x)),
-                        }
+                            x => char::from(x),
+                        };
+                        let mut utf8 = [0; 4];
+                        let n = c.encode_utf8(&mut utf8[..]).len();
+                        name.extend_from_slice(&utf8[..n]);
                     }
                     Ok(())
                 }
@@ -132,10 +142,13 @@ impl Directory {
                 if &entry.name == b".          " || &entry.name == b"..         " {
                     return Ok(());
                 }
-                read_short_name_part(&mut name, &entry.name[..8])?;
+                if &entry.name == b"           " {
+                    return error!("empty file name");
+                }
+                read_short_name_part(&mut self.names, &entry.name[..8])?;
                 if &entry.name[8..] != b"   " {
-                    name.push('.');
-                    read_short_name_part(&mut name, &entry.name[8..])?;
+                    self.names.push(b'.');
+                    read_short_name_part(&mut self.names, &entry.name[8..])?;
                 }
             }
             let r#type = if (attr & ATTR_DIRECTORY) != 0 {
@@ -152,44 +165,37 @@ impl Directory {
                 size,
                 nlink: 1,
             };
-            let id = self.id;
-            self.id += 1;
-            self.entries.insert(id, (name.clone(), info));
-            self.lookup.insert(name, id);
+            self.names.push(0);
+            self.prev_name_end = self.names.len();
+            self.entries.push(DirEntry { name, info })
         }
         Ok(())
     }
-    fn read_from_disk_sector(
-        &mut self,
-        fs: &mut FatFS,
-        sector: u32,
-        long_name: &mut Vec<u8>,
-    ) -> Result<()> {
+    fn read_from_disk_sector(&mut self, fs: &mut FatFS, sector: u32) -> Result<()> {
         let mut data = [0; BLOCK_SECTOR_SIZE];
         fs.block.read(sector, &mut data)?;
         for i in 0..BLOCK_SECTOR_SIZE / 32 {
-            self.read_one_entry(&data[32 * i..32 * (i + 1)], long_name)?;
+            self.read_one_entry(&data[32 * i..32 * (i + 1)])?;
         }
         Ok(())
     }
     pub fn read(fs: &mut FatFS, inode: INodeNum) -> Result<Self> {
         let mut cluster = inode;
         let mut dir = Directory {
-            entries: BTreeMap::new(),
-            lookup: BTreeMap::new(),
-            id: 0,
+            entries: vec![],
+            names: vec![],
+            prev_name_end: 0,
         };
-        let mut long_name = Vec::new();
         if inode == 0 {
             // root directory is special in FAT-16
             // (note: root inode will not be 0 for FAT-32)
             for disk_sector in fs.fat16_root_disk_sectors() {
-                dir.read_from_disk_sector(fs, disk_sector, &mut long_name)?;
+                dir.read_from_disk_sector(fs, disk_sector)?;
             }
         } else {
             loop {
                 for disk_sector in fs.disk_sectors_in_cluster(cluster) {
-                    dir.read_from_disk_sector(fs, disk_sector, &mut long_name)?;
+                    dir.read_from_disk_sector(fs, disk_sector)?;
                 }
                 match fs.fat.entry(cluster) {
                     FatEntry::Defective | FatEntry::Free => {
@@ -203,47 +209,5 @@ impl Directory {
             }
         }
         Ok(dir)
-    }
-    pub fn sync(&mut self, _fs: &mut FatFS) -> Result<()> {
-        // TODO: write changes to disk
-        Ok(())
-    }
-    pub fn entries(&self) -> impl '_ + Iterator<Item = &(String, FileInfo)> {
-        self.entries.values()
-    }
-    pub fn lookup(&self, name: &str) -> Option<INodeNum> {
-        let id = self.lookup.get(name)?;
-        Some(self.entries[&id].1.inode)
-    }
-}
-
-pub struct FatDirectoryIterator<'a> {
-    dir: Result<&'a Directory>,
-    offset: u64,
-}
-
-impl<'a> FatDirectoryIterator<'a> {
-    pub fn new(dir: Result<&'a Directory>, offset: u64) -> Self {
-        Self { dir, offset }
-    }
-}
-
-impl DirectoryIterator for FatDirectoryIterator<'_> {
-    fn next(&mut self) -> Result<Option<DirEntry<'_>>> {
-        let dir = self.dir.as_ref().map_err(|e| e.clone())?;
-        let offset = self.offset;
-        self.offset += 1;
-        Ok(dir
-            .entries
-            .range(offset..)
-            .next()
-            .map(|(_, (name, file_info))| DirEntry {
-                inode: file_info.inode,
-                r#type: file_info.r#type,
-                name,
-            }))
-    }
-    fn offset(&self) -> u64 {
-        self.offset
     }
 }
