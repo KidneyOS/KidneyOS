aa1a7fc3b92b34ee3c3f82b6f75437c81acaba20
Lance Li <lanceli1416@gmail.com>
2024-09-25 21:49:16 -0400

    Refine ata timer

--- kernel/src/drivers/ata/ata_timer.rs
+++ kernel/src/drivers/ata/ata_timer.rs
@@ -4,24 +4,52 @@ use crate::interrupts::timer::sleep;
 use core::arch::asm;
 use core::time::Duration;
 
+const CPU_FREQUENCY_GHZ: u64 = 2;
+
 /// Sleep for `t` milliseconds.
 fn msleep_block(t: u64) {
-    for _ in 0..9 {
-        usleep_block(t);
-    }
+    usleep_block(t * 1000);
 }
 
 /// Sleep for `t` microseconds.
 fn usleep_block(t: u64) {
-    for _ in 0..9 * t {
-        nsleep_block(t);
-    }
+    nsleep_block(t * 1000);
 }
 
 /// Sleep for `t` nanoseconds.
-fn nsleep_block(_t: u64) {
+fn nsleep_block(t: u64) {
+    let cycles_to_wait = t * CPU_FREQUENCY_GHZ;
+
+    let mut start_lo: u32;
+    let mut start_hi: u32;
+    let mut current_lo: u32;
+    let mut current_hi: u32;
+
+    // Get the current value of the time-stamp counter
     unsafe {
-        asm!("nop");
+        asm!(
+        "rdtsc",
+        out("eax") start_lo,
+        out("edx") start_hi
+        );
+    }
+
+    loop {
+        // Get the current value of the time-stamp counter
+        unsafe {
+            asm!(
+            "rdtsc",
+            out("eax") current_lo,
+            out("edx") current_hi
+            );
+        }
+
+        let start = ((start_hi as u64) << 32) | (start_lo as u64);
+        let current = ((current_hi as u64) << 32) | (current_lo as u64);
+
+        if current - start >= cycles_to_wait {
+            return; // Exit the loop once the desired number of cycles has passed
+        }
     }
 }
 
