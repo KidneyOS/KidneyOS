f6272224f3b95648a4c186b22f335faabf45f768
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2024-10-09 23:25:28 -0400

    Run cargo fmt on execve program

--- programs/execve/src/main.rs
+++ programs/execve/src/main.rs
@@ -1,12 +1,13 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
-const TARGET_PROGRAM: &[u8] = include_bytes!("../../example_rust/target/i686-unknown-linux-gnu/release/example_rust");
+const TARGET_PROGRAM: &[u8] =
+    include_bytes!("../../example_rust/target/i686-unknown-linux-gnu/release/example_rust");
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     kidneyos_syscalls::execve(TARGET_PROGRAM.as_ptr(), TARGET_PROGRAM.len());
-    
+
     kidneyos_syscalls::exit(2);
 
     loop {}
