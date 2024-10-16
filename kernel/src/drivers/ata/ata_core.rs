058905253d28987b80008e9ed946b4686af49096
Lance Li <Lanceli1416@gmail.com>
2024-10-16 12:27:54 -0400

    Switch to new sleep mutex

--- kernel/src/drivers/ata/ata_core.rs
+++ kernel/src/drivers/ata/ata_core.rs
@@ -12,9 +12,7 @@ use crate::interrupts::{intr_get_level, IntrLevel};
 use crate::sync::mutex::sleep::SleepMutex;
 use alloc::boxed::Box;
 use alloc::string::String;
-use alloc::vec::Vec;
 use kidneyos_shared::println;
-use lazy_static::lazy_static;
 
 // Commands ----------------------------------------------------------------------------------------
 // Reference: https://wiki.osdev.org/ATA_Command_Matrix
@@ -32,7 +30,7 @@ pub const ATA_IDENTIFY_DEVICE: u8 = 0xEC;
 const CHANNEL_CNT: usize = 2;
 pub static CHANNELS: [SleepMutex<AtaChannel>; CHANNEL_CNT] = [
     SleepMutex::new(AtaChannel::new(0)),
-    SleepMutex::new(AtaChannel::new(1))
+    SleepMutex::new(AtaChannel::new(1)),
 ];
 
 // -------------------------------------------------------------------------------------------------
@@ -88,36 +86,35 @@ pub fn ide_init() {
 /// # Safety
 ///
 /// This function must be called with interrupts enabled
-// unsafe fn identify_ata_device(channel: &mut SleepMutex<AtaChannel>, dev_no: u8, block: bool) {
-unsafe fn identify_ata_device(channel: &SleepMutex<AtaChannel>, dev_no: u8, block: bool) {
+unsafe fn identify_ata_device(c: &SleepMutex<AtaChannel>, dev_no: u8, block: bool) {
     let _index: usize;
-    let c: &mut AtaChannel = &mut channel.lock();
+    let channel: &mut AtaChannel = &mut c.lock();
     let mut id: [u8; BLOCK_SECTOR_SIZE] = [0; BLOCK_SECTOR_SIZE];
 
     // Send the IDENTIFY DEVICE command, wait for an interrupt indicating the device's response
     // is ready, and read the data into our buffer.
-    c.select_device_wait(dev_no, block);
-    c.issue_pio_command(ATA_IDENTIFY_DEVICE);
-    c.sem_down();
+    channel.select_device_wait(dev_no, block);
+    channel.issue_pio_command(ATA_IDENTIFY_DEVICE);
+    channel.sem_down();
 
-    if !c.wait_while_busy(block) {
-        c.set_is_ata(dev_no, false);
+    if !channel.wait_while_busy(block) {
+        channel.set_is_ata(dev_no, false);
         // println!("channel {} device {} is not ata", c.channel_num, dev_no);
         return;
     }
-    c.read_sector(&mut id);
+    channel.read_sector(&mut id);
 
     // Calculate capacity.
     let capacity = usize::from_le_bytes(id[120..124].try_into().unwrap());
     let name = if dev_no == 0 {
-        c.get_d0_name()
+        channel.get_d0_name()
     } else {
-        c.get_d1_name()
+        channel.get_d1_name()
     };
     let name: String = name.iter().collect();
     println!(
         "channel: {} device: {} name: {} capacity: {}M",
-        c.get_channel_num(),
+        channel.get_channel_num(),
         dev_no,
         &name,
         capacity >> 11
@@ -130,6 +127,5 @@ unsafe fn identify_ata_device(channel: &SleepMutex<AtaChannel>, dev_no: u8, bloc
         Box::new(AtaDevice(dev_no)),
     );
 
-    channel.unlock();
     partition_scan(BLOCK_MANAGER.by_id(idx).unwrap());
 }
