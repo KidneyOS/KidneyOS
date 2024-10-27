30d5b0cdad28eb30d5576ebcb55428a101b7d873
Timothy Du <timothydu@Timothys-MacBook-Pro-2.local>
2024-10-26 19:45:26 -0600

    Preliminary addition of subblock_allocator
    This commit includes the addition of the file subblock_allocator_new.rs,
    which contains the implementation for a Fixed Size Subblock Allocator
    based off the following blog post:
    https://os.phil-opp.com/allocator-designs/#discussion-2. Currently, the
    Subblock Allocator has two main issues: it does not support allocations
    larger than one frame (4096 bytes) and is unable to reclaim free frames
    if a frame is no longer in use. Further testing is required to verify
    correctness of allocations.

--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -3,13 +3,12 @@ mod frame_allocator;
 pub mod user;
 pub mod util;
 mod subblock_allocator;
-
+mod subblock_allocator_new;
 
 use alloc::{
     boxed::Box,
     vec::Vec,
 };
-use buddy_allocator::BuddyAllocator;
 use core::{
     alloc::{AllocError, GlobalAlloc, Layout},
     cell::UnsafeCell,
@@ -19,23 +18,19 @@ use core::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 use frame_allocator::{CoreMapEntry, FrameAllocatorSolution, DummyAllocatorSolution};
+use subblock_allocator_new::SubblockAllocator;
 use kidneyos_shared::{
     mem::{virt::trampoline_heap_top, BOOTSTRAP_ALLOCATOR_SIZE, OFFSET, PAGE_FRAME_SIZE},
     println,
     sizes::{KB, MB},
 };
-use crate::mem::subblock_allocator::DumbSubblockAllocator;
 
 
-// Global variables to keep track of allocation statistics
 static TOTAL_NUM_ALLOCATIONS: AtomicUsize = AtomicUsize::new(0);
 static TOTAL_NUM_DEALLOCATIONS: AtomicUsize = AtomicUsize::new(0);
-static TOTAL_NUM_FRAMES_ALLOCATED: AtomicUsize = AtomicUsize::new(0);
-static TOTAL_NUM_FRAMES_DEALLOCATED: AtomicUsize = AtomicUsize::new(0);
 
-// The alignment of the layout cannot be greater than the size of the page
 const MAX_SUPPORTED_ALIGN: usize = 4096;
-// "Upper memory" (as opposed to "lower memory") starts at 1MB.
+/// "Upper memory" (as opposed to "lower memory") starts at 1MB.
 const UPPER_MEMORY_START: usize = MB + OFFSET;
 
 unsafe trait FrameAllocator
@@ -80,13 +75,12 @@ impl FrameAllocatorWrapper{
 }
 
 enum KernelAllocatorState {
-    Deinitialized,
+    DeInitialized,
     SetupState {
         dummy_allocator: DummyAllocatorSolution,
     },
     Initialized {
-        frame_allocator: FrameAllocatorWrapper,
-        subblock_allocators: DumbSubblockAllocator
+        subblock_allocator: SubblockAllocator,
     },
 }
 
@@ -133,121 +127,98 @@ impl KernelAllocator {
             panic!("init called while kernel allocator was already initialized");
         };
 
-        // The exclusive max address is given by multiplying the number of bytes
-        // in a KB by mem_upper, and adding this to UPPER_MEMORY_START.
+        /// The exclusive max address is given by multiplying the number of bytes
+        /// in a KB by mem_upper, and adding this to UPPER_MEMORY_START.
         let frames_ceil_address = UPPER_MEMORY_START.saturating_add(mem_upper * KB);
-        let frames_ceil_pointer = frames_ceil_address as *mut u8;
 
-        // We don't have bootstrap allocator anymore, so the frame base pointer should just be this
-        let frames_base_address = trampoline_heap_top();
-        let frames_base_pointer = frames_base_address as *mut u8;
+        /// TODO: Do we still need to add the BOOTSTRAP_ALLOCATOR_SIZE
+        let frames_base_address = trampoline_heap_top() + BOOTSTRAP_ALLOCATOR_SIZE;
 
-        /*
-        let bootstrap_base = trampoline_heap_top() as *mut u8;
-        let frames_base_pointer = bootstrap_base.add(BOOTSTRAP_ALLOCATOR_SIZE).cast::<u8>();
-
-        // We shouldn't need to define this anymore with the new changes to subblock allocator
-        let bootstrap_allocator = BuddyAllocator::new(NonNull::slice_from_raw_parts(
-            NonNull::new_unchecked(bootstrap_base),
-            BOOTSTRAP_ALLOCATOR_SIZE,
-        ));
-         */
-
-        // Check to see if dummy_allocator initialized properly (both start and end should be zero)
+        /// Check to see if dummy_allocator initialized properly (both start and end should be zero)
         let start = dummy_allocator.get_start_address();
         let end = dummy_allocator.get_end_address();
         assert_eq!(start, 0);
         assert_eq!(end, 0);
 
-        // Set the proper start and end addresses
+        /// Set the proper start and end addresses
         dummy_allocator.set_start_address(frames_base_address);
         dummy_allocator.set_end_address(frames_ceil_address);
 
         let num_frames_in_system = (frames_ceil_address - frames_base_address) /
             (size_of::<CoreMapEntry>() + PAGE_FRAME_SIZE);
 
-        // This should ALWAYS be the first global allocation to take place - should use dummy allocator
-        // This behaviour has not been tested yet, so these print messages should be helpful in debugging
+        /// This should ALWAYS be the first global allocation to take place - should use dummy allocator
+        ///
         println!("Creating Coremap Entries for Frame Allocator");
         let mut core_map: Box<[CoreMapEntry]> = vec![CoreMapEntry::DEFAULT; num_frames_in_system]
                                                 .into_boxed_slice();
         println!("Finished creating Coremap Entries for Frame Allocator");
 
-        // Check that the dummy allocator actually updated its internal state
-        // I.e. the start address should have moved to accommodate Coremap Entries
-        // The coremap should take up 128 frames
+        /// Check that the dummy allocator actually updated its internal state
+        /// I.e. the start address should have moved to accommodate Coremap Entries
+        /// The Coremap should take up 128 frames
+        ///
         assert_ne!(frames_base_address, dummy_allocator.get_start_address());
         println!("Frame Base Address: {}, Dummy Allocator Start Address: {}", frames_base_address, dummy_allocator.get_start_address());
 
-        // With the core_map not initialized, we can now initialize the actual Frame Allocator
+        let mut frame_allocator = FrameAllocatorWrapper::new_in(
+            NonNull::new(dummy_allocator.get_start_address() as *mut u8).expect("frames_base can't be null"),
+            core_map,
+            num_frames_in_system);
+
         *self.state.get_mut() = KernelAllocatorState::Initialized {
-            frame_allocator: FrameAllocatorWrapper::new_in(
-                NonNull::new(dummy_allocator.get_start_address() as *mut u8).expect("frames_base can't be null"),
-                core_map,
-                num_frames_in_system,
-            ),
-            // TODO: Add the constructor for the subblock allocator here
-            subblock_allocators: subblock_allocator::DumbSubblockAllocator::dumb_new()
+            subblock_allocator: SubblockAllocator::new(
+                dummy_allocator.get_start_address() as *mut u8,
+                frame_allocator,
+            )
         };
     }
 
-    /// # Safety
-    ///
-    /// TODO
     pub unsafe fn frame_alloc(&mut self, frames: usize) -> Result<NonNull<[u8]>, AllocError> {
         let KernelAllocatorState::Initialized {
-            frame_allocator, ..
+            subblock_allocator, ..
         } = &mut *self.state.get()
         else {
             return Err(AllocError);
         };
 
-        frame_allocator.alloc(frames)
+        subblock_allocator.get_frame_allocator().alloc(frames)
     }
 
-    /// # Safety
-    ///
-    /// TODO
     pub unsafe fn frame_dealloc(&mut self, ptr: NonNull<u8>) {
         let KernelAllocatorState::Initialized {
-            frame_allocator, ..
+            subblock_allocator, ..
         } = &mut *self.state.get()
         else {
             halt!("Dealloc called on Deinitialized or SetupState kernel allocator");
         };
 
-        frame_allocator.dealloc(ptr);
+        subblock_allocator.get_frame_allocator().dealloc(ptr);
     }
 
 
     pub unsafe fn deinit(&mut self) {
         let KernelAllocatorState::Initialized {
-            subblock_allocators,
-            ..
+            subblock_allocator,
         } = self.state.get_mut()
         else {
             panic!("deinit called before initialization of kernel allocator");
         };
 
         let mut incorrect_num_allocs = false;
-        let mut incorrect_num_frames_allocs = false;
 
         if TOTAL_NUM_ALLOCATIONS.load(Ordering::Relaxed) != TOTAL_NUM_DEALLOCATIONS.load(Ordering::Relaxed) {
             incorrect_num_allocs = true;
         }
 
-        if TOTAL_NUM_FRAMES_ALLOCATED.load(Ordering::Relaxed) != TOTAL_NUM_FRAMES_DEALLOCATED.load(Ordering::Relaxed){
-            incorrect_num_frames_allocs = true;
-        }
+        subblock_allocator.deinit();
 
-        // TODO: Do subblock allocator deinitialization here
-
-        if incorrect_num_allocs || incorrect_num_frames_allocs{
+        if incorrect_num_allocs{
             println!();
             panic!("Leaks detected");
         }
 
-        *self.state.get_mut() = KernelAllocatorState::Deinitialized;
+        *self.state.get_mut() = KernelAllocatorState::DeInitialized;
     }
 }
 
@@ -280,20 +251,17 @@ unsafe impl GlobalAlloc for KernelAllocator {
                                                         / PAGE_FRAME_SIZE;
 
             let Ok(region) = dummy_allocator.alloc(num_frames_requested) else {
-                halt!("Unable to allocate memory according to provided layout, PANIC!");
+                halt!("Unable to allocate memory according to provided layout in DummyAllocator, PANIC!");
             };
 
             // At this point, we know the allocation was successful; increment global statistics
             let new_total_allocs = TOTAL_NUM_ALLOCATIONS.load(Ordering::Relaxed) + 1;
             TOTAL_NUM_ALLOCATIONS.store(new_total_allocs, Ordering::Relaxed);
-            let new_total_frames = TOTAL_NUM_FRAMES_ALLOCATED.load(Ordering::Relaxed) + num_frames_requested;
-            TOTAL_NUM_FRAMES_ALLOCATED.store(new_total_frames, Ordering::Relaxed);
 
             region.as_ptr().cast::<u8>()
         } else {
             let KernelAllocatorState::Initialized {
-                frame_allocator,
-                subblock_allocators: _subblock_allocators,
+                subblock_allocator,
             } = &mut *self.state.get()
                 else {
                     halt!("Second and later allocations should not be allocated by Dummy Allocator, abort");
@@ -310,36 +278,32 @@ unsafe impl GlobalAlloc for KernelAllocator {
             let num_frames_requested = ((size + align).next_multiple_of(PAGE_FRAME_SIZE))
                 / PAGE_FRAME_SIZE;
 
-            // TODO: At this point, try to service the request in the subblock allocator
-            // TODO: If not possible, subblock allocator should call frame_allocator, and then retry the request (this time it should succeed)
+            /// Allocate using subblock allocator
+            let ret_ptr = match subblock_allocator.allocate(layout) {
+                Ok(T) => T,
+                Err(_) => halt!("Unable to allocate memory according to provided layout in SubblockAllocator, PANIC!"),
+            };
 
             // At this point, we know the allocation was successful; increment global statistics
             let new_total_allocs = TOTAL_NUM_ALLOCATIONS.load(Ordering::Relaxed) + 1;
             TOTAL_NUM_ALLOCATIONS.store(new_total_allocs, Ordering::Relaxed);
-            let new_total_frames = TOTAL_NUM_FRAMES_ALLOCATED.load(Ordering::Relaxed) + num_frames_requested;
-            TOTAL_NUM_FRAMES_ALLOCATED.store(new_total_frames, Ordering::Relaxed);
 
-            // Replace this once the subblock allocator is complete
-            ptr::null_mut()
+            ret_ptr
         }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         let KernelAllocatorState::Initialized {
-            frame_allocator,
-            subblock_allocators: _subblock_allocators,
+            subblock_allocator,
         } = &mut *self.state.get()
         else {
             halt!("Dealloc called before initialization of kernel allocator");
         };
 
-        // TODO: Replace this call with a call to subblock allocators free function
-        let num_frames_deallocated = frame_allocator.dealloc(NonNull::new_unchecked(ptr));
+        subblock_allocator.deallocate(ptr, layout);
 
         let new_total_deallocs = TOTAL_NUM_DEALLOCATIONS.load(Ordering::Relaxed) + 1;
         TOTAL_NUM_DEALLOCATIONS.store(new_total_deallocs, Ordering::Relaxed);
-        let new_total_frames = TOTAL_NUM_FRAMES_DEALLOCATED.load(Ordering::Relaxed) + num_frames_deallocated;
-        TOTAL_NUM_FRAMES_DEALLOCATED.store(new_total_frames, Ordering::Relaxed);
     }
 }
 
---
+++ kernel/src/mem/subblock_allocator_new.rs
@@ -0,0 +1,141 @@
+use core::{alloc::{AllocError, Allocator, Layout}, mem::size_of, mem::align_of, ptr, ptr::NonNull, slice};
+use alloc::boxed::Box;
+
+use crate::mem::frame_allocator::{CoreMapEntry, FrameAllocatorSolution};
+use crate::mem::{FrameAllocator, FrameAllocatorWrapper};
+use kidneyos_shared::mem::PAGE_FRAME_SIZE;
+use kidneyos_shared::println;
+
+const SUBBLOCK_TYPE_COUNT: usize = 8;
+/// Allowed subblock sizes, which must be powers of 2.
+const SUBBLOCK_SIZES: [usize; SUBBLOCK_TYPE_COUNT] = [16, 32, 64, 128, 256, 512, 1024, 2048];
+const MAX_NUM_SUBBLOCKS: usize = 256;
+
+/// Returns the smallest subblock size that fits the given number of bytes
+/// in terms of its index in `SUBBLOCK_SIZES`. Returns SUBBLOCK_TYPE_COUNT
+/// if the requested size is larger than the largest subblock size.
+fn get_best_subblock_size_idx(num_bytes: usize) -> usize {
+    if num_bytes > PAGE_FRAME_SIZE {
+        panic!("Requested memory size larger than page frame size");
+    }
+    for (index, size) in SUBBLOCK_SIZES.into_iter().enumerate() {
+        if num_bytes <= size {
+            return index;
+        }
+    }
+    SUBBLOCK_TYPE_COUNT
+}
+
+struct ListNode {
+    next: Option<&'static mut ListNode>,
+}
+
+pub struct SubblockAllocator {
+    list_heads: [Option<&'static mut ListNode>; SUBBLOCK_TYPE_COUNT],
+    frame_allocator: FrameAllocatorWrapper,
+    mem_start: *mut u8
+}
+
+impl SubblockAllocator {
+    pub fn new(mem_start: *mut u8, frame_allocator: FrameAllocatorWrapper) -> Self {
+        const EMPTY: Option<&'static mut ListNode> = None;
+
+        SubblockAllocator {
+            list_heads: [EMPTY; SUBBLOCK_TYPE_COUNT],
+            mem_start,
+            frame_allocator
+        }
+    }
+
+    /// Allocate a fixed size block for the size requested by 'layout'
+    ///
+    pub fn allocate(&mut self, layout: Layout) -> Result<*mut u8, AllocError> {
+        let subblock_size_index = get_best_subblock_size_idx(layout.size());
+
+        if (subblock_size_index == SUBBLOCK_TYPE_COUNT){
+            println!("Size requested larger than one frame, size not supported currently");
+            return Err(AllocError);
+        };
+
+        match self.list_heads[subblock_size_index].take(){
+            Some(node) => {
+                // Set the new list head to be the next node from the current node
+                //
+                // Think of this as doing a linked_list.pop_from_head() operation
+                println!("List head exists, allocating subblock");
+                self.list_heads[subblock_size_index] = node.next.take();
+                Ok(node as *mut ListNode as *mut u8)
+            }
+            None => {
+                /// If no block currently exists, we need to allocate a frame and divide the frame
+                ///
+                /// We first have to make sure the subblock size has enough room to hold a ListNode
+                /// This should always be the case, but we check regardless
+                println!("List head does not exist, requesting frame");
+                assert!(size_of::<ListNode>() <= SUBBLOCK_SIZES[subblock_size_index]);
+
+                let mut new_frame = match self.frame_allocator.alloc(1) {
+                    Err(AllocError) => return Err(AllocError),
+                    Ok(v) => v
+                };
+                let start_of_frame_addr = new_frame.as_ptr() as *const u8 as usize;
+                let num_subblocks = PAGE_FRAME_SIZE / SUBBLOCK_SIZES[subblock_size_index];
+
+                /// Begin to divide the frame into the required subblock sizes
+                for i in 0..num_subblocks {
+                    let start_of_subblock_addr = start_of_frame_addr + (SUBBLOCK_SIZES[subblock_size_index] * i);
+                    let start_of_subblock_ptr = start_of_subblock_addr as *mut u8 as *mut ListNode;
+
+                    let next = self.list_heads[subblock_size_index].take();
+                    let new_node = ListNode{ next };
+
+                    unsafe {
+                        start_of_subblock_ptr.write(new_node);
+                        self.list_heads[subblock_size_index] = Some(&mut *start_of_subblock_ptr)
+                    }
+                }
+
+                /// At this point, the head of the linked list should not be None
+                assert_eq!(self.list_heads[subblock_size_index].is_none(), false);
+
+                /// Return the head of the linked list
+                let node = self.list_heads[subblock_size_index].take().unwrap();
+                self.list_heads[subblock_size_index] = node.next.take();
+                Ok(node as *mut ListNode as *mut u8)
+            }
+        }
+    }
+
+    /// Deallocate the region of memory pointed to by 'ptr' with size 'layout'
+    ///
+    /// When we deallocate, we simply write a new ListNode to the pointer we are given and
+    /// make the new ListNode the head of the linked list
+    ///
+    /// This is very similar to linked_list.insert_head() operation
+    pub fn deallocate(&mut self, ptr: *mut u8, layout: Layout) {
+        let subblock_size_index = get_best_subblock_size_idx(layout.size());
+
+        assert!(size_of::<ListNode>() <= SUBBLOCK_SIZES[subblock_size_index]);
+
+        let new_node = ListNode {
+            next: self.list_heads[subblock_size_index].take(),
+        };
+
+        let new_node_ptr = ptr as *mut ListNode;
+        unsafe {
+            new_node_ptr.write(new_node);
+            self.list_heads[subblock_size_index] = Some(&mut *new_node_ptr);
+        }
+    }
+
+    /// Uninitialize the subblock and frame allocator
+    /// TODO
+    pub fn deinit(&mut self) -> bool{
+        true
+    }
+
+    /// Return mutable reference to frame allocator
+    pub fn get_frame_allocator(&mut self) -> &mut FrameAllocatorWrapper {
+        &mut self.frame_allocator
+    }
+}
