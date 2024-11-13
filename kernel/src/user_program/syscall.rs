8b9c3c22b53461d9e38fe7690e7ddd793c33cf6b
Nicholas McKee <nmsmckee@gmail.com>
2024-11-13 13:53:56 -0500

    Fix issue with unsigned arg

--- kernel/src/user_program/syscall.rs
+++ kernel/src/user_program/syscall.rs
@@ -7,7 +7,7 @@ use crate::fs::syscalls::{
 use crate::interrupts::{intr_disable, intr_enable};
 use crate::mem::user::check_and_copy_user_memory;
 use crate::mem::util::get_mut_from_user_space;
-use crate::system::{running_thread_pid, running_thread_ppid, running_thread_tid, unwrap_system};
+use crate::system::{running_thread_pid, running_thread_ppid, running_thread_tid, unwrap_system, running_process};
 use crate::threading::process::Pid;
 use crate::threading::process_functions;
 use crate::threading::scheduling::{scheduler_yield_and_continue, scheduler_yield_and_die};
@@ -57,10 +57,7 @@ pub extern "C" fn handler(syscall_number: usize, arg0: usize, arg1: usize, arg2:
         SYS_MOUNT => mount(arg0 as _, arg1 as _, arg2 as _),
         SYS_SYNC => sync(),
         SYS_WAITPID => {
-            let wait_pid = match arg0 {
-                0 => running_thread_ppid(),
-                _ => arg0 as Pid,
-            };
+            let wait_pid = arg0 as Pid;
 
             if wait_pid == running_thread_pid() {
                 return -1;
@@ -72,33 +69,28 @@ pub extern "C" fn handler(syscall_number: usize, arg0: usize, arg1: usize, arg2:
             };
 
             let system = unwrap_system();
-
             let pcb_ref = system.process.table.get(wait_pid).unwrap();
             let mut parent_pcb = pcb_ref.lock();
 
+            // Can't wait on a thread that alreay has a child waiting
             if parent_pcb.waiting_thread.is_some() {
                 return -1;
             }
 
             parent_pcb.waiting_thread = Some(running_thread_tid());
 
-            // Disable interrupts for atomic operations
+            // Disable interrupts as this must be done atomically
             intr_disable();
             if parent_pcb.exit_code.is_none() {
                 thread_sleep();
             }
             intr_enable();
 
-            // Set the status pointer based on the exit code
             *status_ptr = (parent_pcb.exit_code.unwrap() & 0xff) << 8;
 
-            // Retrieve the pid to be removed
             let parent_pid = parent_pcb.pid;
-
-            // Now we can remove the entry from the table
             system.process.table.remove(parent_pid);
 
-            println!("exiting wait syscall");
             parent_pid as isize
         }
         SYS_EXECVE => {
