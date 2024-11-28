3bd1306051c8291805239b56722d24a391f2ca56
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2024-11-28 16:39:46 -0500

    Run cargo fmt on clone program

--- programs/clone/src/main.rs
+++ programs/clone/src/main.rs
@@ -5,8 +5,14 @@ use kidneyos_syscalls::arguments::RawArguments;
 
 #[no_mangle]
 pub extern "C" fn _start(_raw: RawArguments) -> ! {
-    kidneyos_syscalls::clone(0, core::ptr::null_mut(), core::ptr::null_mut(), 0, core::ptr::null_mut());
-    
+    kidneyos_syscalls::clone(
+        0,
+        core::ptr::null_mut(),
+        core::ptr::null_mut(),
+        0,
+        core::ptr::null_mut(),
+    );
+
     kidneyos_syscalls::exit(0);
 }
 
